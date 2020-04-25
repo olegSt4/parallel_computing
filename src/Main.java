adbd99cb2a99c630873d49d95b2fe5589ffaae53
olegSt4 <stefura2010@ukr.net>
2020-04-25 20:25:58 +0300

    feat(files): files loading is added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        File firstDir = new File("files//test");
+        File secondDir = new File("files//train");
+
+        File[] filesTest = firstDir.listFiles();
+        File[] fileTrain = secondDir.listFiles();
+
     }
 }
