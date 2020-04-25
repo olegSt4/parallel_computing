e4d41c3b6f4a3a6ce6569f6a6817143ff11ee3f1
olegSt4 <stefura2010@ukr.net>
2020-04-25 21:36:55 +0300

    feat: secure threads num request is added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -9,5 +11,20 @@ public class Main {
         File[] filesTest = firstDir.listFiles();
         File[] fileTrain = secondDir.listFiles();
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the num of threads (from 1 to 100) or 0 to exit: ");
+
+        int threadsNum = 0;
+        while(true) {
+            String input = scan.next();
+            if(input.matches("\\d+")) {
+                if(Integer.valueOf(input) >= 0 && Integer.valueOf(input) <= 100) {
+                    threadsNum = Integer.valueOf(input);
+                    break;
+                }
+            }
+            System.out.println("The input is incorrect! Try one more time.");
+            System.out.println("(Enter the num of threads from 1 to 100 or 0 to exit)");
+        }
     }
 }
