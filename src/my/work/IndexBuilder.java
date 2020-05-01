8a416f6805cde898ad7be1aee55f00e4d9af89b7
olegSt4 <stefura2010@ukr.net>
2020-05-01 17:33:57 +0300

    refactor: IndexBuilder class is refactored

--- src/my/work/IndexBuilder.java
+++ src/my/work/IndexBuilder.java
@@ -2,22 +2,37 @@ package my.work;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
+/**
+ * This class describes the work of one thread in terms of multi-thread processing
+ */
 public class IndexBuilder extends Thread {
     private static final int START = 0;
     private static final int END = 1;
 
+    /** All files*/
     private File[][] parts;
+
+    /** Special boundaries of corresponding parts for this thread*/
     private int[][] bounds;
+
+    /** The result of thread's work will be written here*/
     private HashMap<String, List<String>> blockIndex;
+
     private int threadId;
 
-    public IndexBuilder(File[][] p, int[][] b, HashMap<String, List<String>> bi, int thi) {
-        parts = p;
-        bounds = b;
-        blockIndex = bi;
-        threadId = thi;
+    public IndexBuilder(File[][] parts, int[][] bounds, HashMap<String, List<String>> blockIndex, int threadId) {
+        this.parts = parts;
+        this.bounds = bounds;
+        this.blockIndex = blockIndex;
+        this.threadId = threadId;
     }
 
     @Override
@@ -26,16 +41,15 @@ public class IndexBuilder extends Thread {
             System.out.println("Thread " + threadId + " started");
             Scanner scan;
 
-            for(int i = 0; i < parts.length; i++) {
-                for (int j = bounds[i][START]; j < bounds[i][END]; j++) {
-                    String fileName = parts[i][j].getName().replaceAll(".txt", "");
-                    scan = new Scanner(parts[i][j]);
+            for (int partNum = 0; partNum < parts.length; partNum++) {
+                for (int fileNum = bounds[partNum][START]; fileNum < bounds[partNum][END]; fileNum++) {
+                    String fileName = parts[partNum][fileNum].getName().replaceAll(".txt", "");
+                    scan = new Scanner(parts[partNum][fileNum]);
 
                     while (scan.hasNext()) {
                         String input = scan.nextLine();
-                        input = input.replaceAll("\\d+", "")
-                                .replaceAll("<br />", " ")
-                                .replaceAll("[^A-Za-zА-Яа-я0-9\\s]", "")
+                        input = input.replaceAll("<br />", " ")
+                                .replaceAll("[^A-Za-z\\s]", "")
                                 .replaceAll(" +", " ")
                                 .trim()
                                 .toLowerCase();
@@ -43,18 +57,18 @@ public class IndexBuilder extends Thread {
                         Queue<String> words = new PriorityQueue<>(Arrays.asList(input.split(" ")));
 
                         String prev = "";
-                        while(words.size() != 0) {
+                        while (words.size() != 0) {
                             String word = words.poll();
-                            if(word.compareTo(prev) == 0 || word.length() == 1) {
+                            if (word.compareTo(prev) == 0 || word.length() == 1) {
                                 continue;
                             }
 
-                            if(blockIndex.containsKey(word)) {
-                                blockIndex.get(word).add(i + ":" + fileName);
+                            if (blockIndex.containsKey(word)) {
+                                blockIndex.get(word).add(partNum + ":" + fileName);
                             } else {
-                                List<String> newList = new LinkedList<>();
-                                newList.add(i + ":" + fileName);
-                                blockIndex.put(word, newList);
+                                List<String> wordPositions = new LinkedList<>();
+                                wordPositions.add(partNum + ":" + fileName);
+                                blockIndex.put(word, wordPositions);
                             }
                             prev = word;
                         }
@@ -63,7 +77,6 @@ public class IndexBuilder extends Thread {
             }
             System.out.print("Block's InvertedIndex has been successfully built in thread " + threadId);
             System.out.println(" (The size is: " + blockIndex.size() + ")");
-
         } catch (FileNotFoundException ex) {
             System.out.println("On of the files is not found! (thread " + threadId + ")");
         }
